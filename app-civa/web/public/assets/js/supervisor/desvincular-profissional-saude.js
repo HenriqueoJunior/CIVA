5a6d3e7a0de5c7b2e680575e16dd2861cb9ebd94
Antonio Henrique <78893552+HenriqueoJunior@users.noreply.github.com>
2021-11-20 18:56:44 -0300

    Js atualizado

---
+++ app-civa/web/public/assets/js/supervisor/desvincular-profissional-saude.js
@@ -0,0 +1,32 @@
+$('.select2').select2();
+
+let campos = ['identity', 'name', 'surname', 'genero', 'date-birth', 'nacionality', 'tipo-doc', 'doc'];
+
+
+let form = $("#form-meus-dados");
+
+
+$("#desvincular").click(function () {
+
+    if (tratar_campos(campos)) {
+        $.get("", form.serialize(), (data, status) => {
+            if (status === 'success') {
+                title = 'Profissional de Sa&uacute;de desvinculado com sucesso!';
+                text = "Desvinculaç&atilde;o realizado.";
+                swalAlertSuccess(title, text, callback);
+                
+            } else {
+                title = 'Erro!';
+                text = 'C&oacute;digo CIVA n&atilde;o encontrado.';
+                swalAlertError(title, text, callback);
+            }
+        });
+    } else {
+        title = 'Preencha o c&oacute;digo CIVA!';
+        text = 'C&oacute;digo CIVA do Profissional de Sa&uacutede deve ser preenchido!';
+        swalAlertError(title, text, callback);
+    }
+});
+
+
+pegarPaises("nacionalidade");
